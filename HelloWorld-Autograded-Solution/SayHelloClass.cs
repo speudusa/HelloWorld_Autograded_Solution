2ca9528407302106af416bec903b63dc05eea651
Courtney Frey <courtneyb.frey@gmail.com>
2023-01-05 14:18:46 -0600

    solution code created

---
+++ HelloWorld-Autograded-Solution/SayHelloClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace HelloWorld_Autograded_Solution
+{
+	public class SayHelloClass
+	{
+
+        //Update the method to pass the auto-graded test
+
+        public static string SayHello()
+        {
+            // ** changed the return value here **
+            return "Hello, World!";
+        }
+
+        //class constructor - intentionally empty
+        public SayHelloClass()
+        {
+
+        }
+    }
+}
+
